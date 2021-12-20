14f264ec97acd513608ec002550d2ba5776ac565
stoleriudaniel <stoleriu.daniel47@gmail.com>
2021-12-20 18:51:18 +0200

    Create client handler

--- Server.py
+++ Server.py
@@ -1,16 +1,29 @@
 import socket
+import threading
 
 IP = socket.gethostbyname(socket.gethostname())
 PORT = 5480
 ADDR = (IP, PORT)
 TEXT_MAXIMUM_SIZE = 1024
 FORMAT = "utf-8"
-DISCONNECT_MSG = "EXIT"
+EXIT_MESSAGE = "EXIT"
 CONN_ACCEPTED_MESSAGE = "[SERVER] Connection accepted. Welcome!"
 CONN_FAILED_MESSAGE = "[SERVER] Connection failed! Server is full. Try again later."
 MAXIMUM_CONNECTIONS = 3
 CURR_CONNECTIONS = 0
 
+
+def handle_client(conn, addr):
+    global CURR_CONNECTIONS
+    connected = True
+    while connected:
+        message = conn.recv(TEXT_MAXIMUM_SIZE).decode(FORMAT)
+        if message == EXIT_MESSAGE:
+            connected = False
+        conn.send(message.encode(FORMAT))
+    conn.close()
+    CURR_CONNECTIONS = CURR_CONNECTIONS - 1
+    print(f"[SERVER] Active connections: {CURR_CONNECTIONS}")
 
 def server():
     print("[SERVER] Starting...")
@@ -20,15 +33,16 @@ def server():
     print(f"[SERVER] Server is listening on {IP}:{PORT}")
     global CURR_CONNECTIONS
     while True:
+        conn, addr = serv.accept()
         if CURR_CONNECTIONS < MAXIMUM_CONNECTIONS:
-            conn, addr = serv.accept()
             CURR_CONNECTIONS = CURR_CONNECTIONS + 1
             print(f"[SERVER] Active connections: {CURR_CONNECTIONS}")
             conn.send(CONN_ACCEPTED_MESSAGE.encode(FORMAT))
             if CURR_CONNECTIONS == MAXIMUM_CONNECTIONS:
                 print("[SERVER] Maximum connections achieved")
+            thread = threading.Thread(target=handle_client, args=(conn, addr))
+            thread.start()
         else:
-            conn, addr = serv.accept()
             conn.send(CONN_FAILED_MESSAGE.encode(FORMAT))
             conn.close()
 
