debf9aec2c57717ee893e3e297d42b449be2f6c5
stoleriudaniel <stoleriu.daniel47@gmail.com>
2021-12-21 13:34:03 +0200

    Choose the winner and print rounds

--- Server.py
+++ Server.py
@@ -14,7 +14,7 @@ MAXIMUM_CONNECTIONS = 3
 CURR_CONNECTIONS = 0
 
 possible_options = ["ROCK", "PAPER", "SCISSORS", "LIZARD", "SPOCK"]
-
+players = ["SERVER", "CLIENT"]
 
 def valid_option(option):
     return option in possible_options
@@ -35,10 +35,11 @@ def option1_bigger_than_option2(option1, option2):
 
 
 def handle_client(conn, addr):
-    global CURR_CONNECTIONS
+    global CURR_CONNECTIONS, message
     rounds = []
+    winner = -1
     connected = True
-    while connected:
+    while connected and winner == -1:
         client_option = conn.recv(TEXT_MAXIMUM_SIZE).decode(FORMAT)
         if client_option == EXIT_MESSAGE:
             connected = False
@@ -46,11 +47,26 @@ def handle_client(conn, addr):
             message = "Your option is not valid!"
             conn.send(message.encode(FORMAT))
         elif valid_option(client_option) is True:
-            message = "Your option is valid!"
-            conn.send(message.encode(FORMAT))
             random_index = random.randint(0, 4)
             server_option = possible_options[random_index]
-
+            rounds.append([server_option, client_option])
+            if option1_bigger_than_option2(server_option, client_option):
+                winner = "SERVER"
+                print("--SERVER")
+            elif option1_bigger_than_option2(client_option, server_option):
+                winner = "CLIENT"
+                print("--CLIENT")
+            if winner != -1:
+                if winner == "SERVER":
+                    message = f"Server choose {server_option}. You lose!.\n"
+                if winner == "CLIENT":
+                    message = f"Server choose {server_option}. You win!\n"
+                message = message + "Rounds([SERVER, CLIENT]):\n"
+                for round in rounds:
+                    message = message + f"{round}"
+            conn.send(message.encode(FORMAT))
+    print("winner:", winner)
+    print("rounds:", rounds)
     conn.close()
     CURR_CONNECTIONS = CURR_CONNECTIONS - 1
     print(f"[SERVER] Active connections: {CURR_CONNECTIONS}")
